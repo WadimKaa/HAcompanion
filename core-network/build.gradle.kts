06eda1c03f3783207dffad9830941d6496215cb3
wadim <112875224+WadimKaa@users.noreply.github.com>
2026-05-06 15:44:03 +0300

    add net skelet and safe calls

--- core-network/build.gradle.kts
+++ core-network/build.gradle.kts
@@ -14,10 +14,9 @@ android {
 
 dependencies {
     implementation(libs.retrofit)
-
     implementation(libs.kotlinx.serialization.json)
     implementation(libs.retrofit.serialization)
-
     implementation(libs.okhttp)
     implementation(libs.okhttp.logging)
+    implementation(libs.coroutines.core)
 }
