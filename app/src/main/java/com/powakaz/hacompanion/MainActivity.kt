ca2eb3fcdd3235845e60bea6a4866bb5c6a81523
wadim <112875224+WadimKaa@users.noreply.github.com>
2026-05-11 10:13:48 +0300

    feat(chore): drop trash code

--- app/src/main/java/com/powakaz/hacompanion/MainActivity.kt
+++ app/src/main/java/com/powakaz/hacompanion/MainActivity.kt
@@ -37,34 +37,6 @@ class MainActivity : ComponentActivity() {
             }
         }
 
-        val baseUrl = "http://192.168.0.11:8123/"
-        val token =
-            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJjZDllNzA0NTFkYjU0MGJjOTJkMDViY2FjYjI2YmIwMyIsImlhdCI6MTc3NzkwMzQyNSwiZXhwIjoyMDkzMjYzNDI1fQ.tTdwBmmGl2JS5WFq_XHdaE7oJMwGxBDluSjZfgfLHqI"
-
-        val authInterceptor = AuthInterceptor(token)
-        val okHttpClient = NetworkFactory.createOkHttpClient(authInterceptor)
-
-        val retrofit = RetrofitFactory.createRetrofit(baseUrl, okHttpClient)
-        val api = RetrofitFactory.createApi<NetworkTodoListApi>(retrofit)
-
-        lifecycleScope.launch {
-            val result = safeApiCall { api.getTodoItems(TodoRequest("todo.moi_dela")) }
-            when (result) {
-                is NetworkResult.Success -> {
-                    // Данные пришли успешно
-                    val items = result.data
-                    println("Успех! Найдено задач: ${items.size}")
-                }
-                is NetworkResult.Error -> {
-                    // Ошибка API (например, 401 или 404)
-                    println("Ошибка сервера: ${result.code} - ${result.message}")
-                }
-                is NetworkResult.Exception -> {
-                    // Проблемы с сетью или парсингом (таймаут, нет интернета)
-                    println("Исключение: ${result.e.message}")
-                }
-            }
-        }
     }
 }
 
