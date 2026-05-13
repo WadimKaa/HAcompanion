58137c469d10bddabb66974cc414aed0d2e9b751
Polina <128475862+yakhnovets@users.noreply.github.com>
2026-05-13 11:54:40 +0300

    feat(shopping): add start usecase

---
+++ feature-shopping/src/main/java/com/powakaz/feature_shopping/di/ShoppingRepositoryModule.kt
@@ -0,0 +1,19 @@
+package com.powakaz.feature_shopping.di
+
+import com.powakaz.feature_shopping.data.repository.ShoppingRepositoryImpl
+import com.powakaz.feature_shopping.domain.repository.ShoppingRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class ShoppingRepositoryModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindShoppingRepository(shoppingRepositoryImpl: ShoppingRepositoryImpl): ShoppingRepository
+}
